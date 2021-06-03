2ec0cfb536ee3c6e17288546e183549903e9a7b6
Hamzah <hamzahullah@yahoo.com>
2021-06-03 18:28:39 -0400

    Uses API currency value to convert crypto value to USD value conversion
    Currently affixed with dummy values at lines 8,9 and 10.
    line 8, Value is the initial crypto variable of user
    line 9, increment is theoretical on click increment amount
    line 10, is the value of converted amount summed
    Currently on an interval every 2 seconds to observe price

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,20 @@ var cryptoCurrency = "DOGE,ETH,BTC";
 const currencies = "USD"; 
 var CRYPTO_USD_PRICE_API = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptoCurrency}&tsyms=${currencies}&api_key=${API_KEY}`
 var USD_CRYPTO_PRICE_API = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${currencies}&tsyms=${cryptoCurrency}&api_key=${API_KEY}`
+//used in cryptoToUSD function
+var value = 0
+var increment = .25
+var usdConversion = 0
 
 function cryptoToUSD(CRYPTO_USD_PRICE_API)
 {
     fetch(CRYPTO_USD_PRICE_API).then(function(response){
-        console.log(response.json())
+        return response.json()
     }).then(function(data){
-        console.log(data)
+        value += increment; 
+        console.log(value,usdConversion)
+        usdConversion = value.toFixed(5) * data.BTC.USD.toFixed(5)
+        console.log(value,usdConversion)
     })
 }
 function USDToCrypto(USD_CRYPTO_PRICE_API)
@@ -22,6 +29,8 @@ function USDToCrypto(USD_CRYPTO_PRICE_API)
         
     });
 }
+setInterval(function(){
+    cryptoToUSD(CRYPTO_USD_PRICE_API)
+},2000)
 
-cryptoToUSD(CRYPTO_USD_PRICE_API)
 USDToCrypto(USD_CRYPTO_PRICE_API)
