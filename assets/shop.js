3f676a1265d3db9d7a47c52c1a5bba62b16ea8cc
AnthonySzymczak <doyalajs1@live.com>
2021-06-03 16:39:53 -0400

    setting up comments to guide code

--- assets/shop.js
+++ assets/shop.js
@@ -17,26 +17,61 @@ $(document).ready(function () {
 */  
 
 /*                                      Video Cards
+    ___________________________
+1)  Asus gt 1030 2gb:     $125||
+    __________________________||
+    on.click addTo(current build).removeBuild
 
-1)  Asus gt 1030 2gb:     $125		
-2)  Asus GTX 1050Ti:      $290		
-3)  Asus RTX 2060:          $610		
-4)  Asus 6700 XT:            $1300		
-5)  Saphire RX 6900:      $2310		
-6)  NVIDIA RTX 3080Ti: $5000		
 
+    ____________________________
+2)  Asus GTX 1050Ti:      $290||
+    __________________________||
+    on.click addTo(current build).removeBuild
+
+    ____________________________
+3)  Asus RTX 2060:        $610||
+    __________________________||
+    on.click addTo(current build).removeBuild	
+        
+        
+    ____________________________
+4)  Asus 6700 XT:       $1300 ||
+    __________________________||		
+    on.click addTo(current build).removeBuild
+
+
+    _____________________________
+5)  Saphire RX 6900:      $2310||
+    ___________________________||		
+    on.click addTo(current build).removeBuild
+
+
+    _________________________
+6)  NVIDIA RTX 3080Ti: $5000   ||
+    ___________________________||		
+
+    on.click addTo(current build).removeBuild
 */
 
-/*                                      Processors 
+//                                      Processors 
 
 
-*/
 
 
-/*                                     Motherboards 
+
+//                                     Motherboards 
 
 
-*/
+
+
+
+/*        := Checkout =:
+When you click ‘checkout’ button,
+ A prompt will appear:		“Finished shopping?”
+UserResponseInput  : “yes”, “no” (or on.click ok/cancel)
+After purchase:
+Alert: “Good luck mining!”
+ */
 
 
 
