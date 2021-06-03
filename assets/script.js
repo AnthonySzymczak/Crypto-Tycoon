c6d31d6ca4acf54c28854dd2179ec3fd575c1950
Hamzah Ullah <82611798+brobro10000@users.noreply.github.com>
2021-06-03 18:45:10 -0400

    Checked CryptoCompare API for usability. (#14)
    * Checked CryptoCompare API for usability.
    
    Offers conversion rates of cryptocurrencies and multiple currencies at once.
    
    * Uses API currency value to convert crypto value to USD value conversion
    
    Currently affixed with dummy values at lines 8,9 and 10.
    line 8, Value is the initial crypto variable of user
    line 9, increment is theoretical on click increment amount
    line 10, is the value of converted amount summed
    Currently on an interval every 2 seconds to observe price

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
-console.log("This is dummy code")
+//DOGE - DOGE COIN BTC - BITCOIN ETH - ETHEREUM
+const API_KEY = "0a4a30fd69c551af6529573e0770da441e7496f29fade17e52b1b78e221a3444"
+var cryptoCurrency = "DOGE,ETH,BTC";
+const currencies = "USD"; 
+var CRYPTO_USD_PRICE_API = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptoCurrency}&tsyms=${currencies}&api_key=${API_KEY}`
+var USD_CRYPTO_PRICE_API = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${currencies}&tsyms=${cryptoCurrency}&api_key=${API_KEY}`
+//used in cryptoToUSD function
+var value = 0
+var increment = .25
+var usdConversion = 0
+
+function cryptoToUSD(CRYPTO_USD_PRICE_API)
+{
+    fetch(CRYPTO_USD_PRICE_API).then(function(response){
+        return response.json()
+    }).then(function(data){
+        value += increment; 
+        console.log(value,usdConversion)
+        usdConversion = value.toFixed(5) * data.BTC.USD.toFixed(5)
+        console.log(value,usdConversion)
+    })
+}
+function USDToCrypto(USD_CRYPTO_PRICE_API)
+{
+    fetch(USD_CRYPTO_PRICE_API).then(function(response){
+        console.log(response.json())
+    }).then(function(data){
+        console.log(data)
+        
+    });
+}
+setInterval(function(){
+    cryptoToUSD(CRYPTO_USD_PRICE_API)
+},2000)
+
+USDToCrypto(USD_CRYPTO_PRICE_API)
