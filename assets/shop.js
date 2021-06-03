fae885a1788f4fb91a1bcd0bd5564e7f1418eb02
AnthonySzymczak <doyalajs1@live.com>
2021-06-03 19:02:59 -0400

    cleaning up

--- assets/shop.js
+++ assets/shop.js
@@ -18,59 +18,43 @@ $(document).ready(function () {
 
 /*                                      Video Cards
     ___________________________
-1)  Asus gt 1030 2gb:     $125||
-    __________________________||
-    on.click addTo(current build).removeBuild
-
-
+1)  Asus gt 1030 2gb:     $125
+   on.click addTo(current build).removeBuild
     ____________________________
-2)  Asus GTX 1050Ti:      $290||
-    __________________________||
+2)  Asus GTX 1050Ti:      $290
     on.click addTo(current build).removeBuild
-
     ____________________________
-3)  Asus RTX 2060:        $610||
-    __________________________||
-    on.click addTo(current build).removeBuild	
-        
-        
+3)  Asus RTX 2060:        $610
+    on.click addTo(current build).removeBuild	    
     ____________________________
-4)  Asus 6700 XT:       $1300 ||
-    __________________________||		
+4)  Asus 6700 XT:       $1300
     on.click addTo(current build).removeBuild
-
-
     _____________________________
-5)  Saphire RX 6900:      $2310||
-    ___________________________||		
+5)  Saphire RX 6900:      $2310
     on.click addTo(current build).removeBuild
-
-
-    _________________________
-6)  NVIDIA RTX 3080Ti: $5000   ||
-    ___________________________||		
-
+  _______________________________
+6)  NVIDIA RTX 3080Ti: $5000   	
     on.click addTo(current build).removeBuild
 */
 
 //                                      Processors 
 /*
-1) AMD ryzen 3: $200
-2)AMD ryzen 5: $300
-3)AMD ryzen 7:    $400
-4)AMD ryzen 9 :   $800
-5)AMD threadripper 3960x :$1500
-6) THA SHREDDER: $5000
+1) AMD ryzen 3:             $200
+2)AMD ryzen 5:              $300
+3)AMD ryzen 7:              $400
+4)AMD ryzen 9 :             $800
+5)AMD threadripper 3960x :  $1500
+6) THA SHREDDER:            $5000
 */
 
 //                                     Motherboards 
 /*
-1)ASUS b450: $150 
-2)ASUS b550: $250
-3)ASUS x570:$350
+1)ASUS b450:        $150 
+2)ASUS b550:        $250
+3)ASUS x570:        $350
 4)ASUS Godlike 570: $700
-5)AORUS XTREME: $2000
-6)MEGAVBOARD: $5000
+5)AORUS XTREME:     $2000
+6)MEGAVBOARD:       $5000
 
 
 */
