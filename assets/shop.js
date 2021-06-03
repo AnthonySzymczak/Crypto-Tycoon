964cd9a437a820450223b3d571c9adcee30edace
AnthonySzymczak <doyalajs1@live.com>
2021-06-03 16:32:23 -0400

    setting up shop declarations

---
+++ assets/shop.js
@@ -0,0 +1,47 @@
+/* In this attached file you will see an interactive shop user interface 
+    you will notice a clean interface with many purchaseable upgrades */
+
+//Calls document first
+$(document).ready(function () {
+
+    /* welcome header */
+
+/*
+                                    <h1> Welcome to computer plus</h1>
+                            <h2>Where we have all your mining needs!</h2>
+                        <h3>18 incredible upgrades to increase your mining rate!</h3>
+*/
+
+/*                      create a table populated with 18 upgradeable options
+                                      three categories: 
+*/  
+
+/*                                      Video Cards
+
+1)  Asus gt 1030 2gb:     $125		
+2)  Asus GTX 1050Ti:      $290		
+3)  Asus RTX 2060:          $610		
+4)  Asus 6700 XT:            $1300		
+5)  Saphire RX 6900:      $2310		
+6)  NVIDIA RTX 3080Ti: $5000		
+
+*/
+
+/*                                      Processors 
+
+
+*/
+
+
+/*                                     Motherboards 
+
+
+*/
+
+
+
+
+
+
+
+})
