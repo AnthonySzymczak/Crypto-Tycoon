c12c683093a35a5cdc2cd2981bdac3d5996e6984
AnthonySzymczak <doyalajs1@live.com>
2021-06-03 20:16:27 -0400

    cleaning up js

--- assets/shop.js
+++ assets/shop.js
@@ -4,88 +4,30 @@
 //Calls document first
 $(document).ready(function () {
 
-    /* welcome header */
+//Savebutton/ local storage setup
+$(".button").on("click",function(){
+    for(let i=1; i <= 18; i++)
+    save(i);
+})
 
-/*
-                                    <h1> Welcome to computer plus</h1>
-                            <h2>Where we have all your mining needs!</h2>
-                        <h3>18 incredible upgrades to increase your mining rate!</h3>
-*/
+let storedItem = localStorage.getItem("storedItem");
 
-/*                      create a table populated with 18 upgradeable options
-                                      three categories: 
-*/  
+function save(i){
+    let item = (document.getElementById("text" +i).value);
+    console.log(item);
+    localStorage.setItem("storedItem" +i, item);
+    
+    console.log(storedItem);
+}
 
-/*                                      Video Cards
-1)  Asus gt 1030 2gb:     $125
-   on.click addTo(current build).removeBuild
-   */
-
-
-   /*
-2)  Asus GTX 1050Ti:      $300
-    on.click addTo(current build).removeBuild
-*/
-
-
-/*
-    3)  Asus RTX 2060:        $650
-    on.click addTo(current build).removeBuild	    
-  
-*/
-
-
-/*    4)  Asus 6700 XT:       $1300
-    on.click addTo(current build).removeBuild
- */
-
-/*    _____________________________
-5)  Saphire RX 6900:      $2600
-    on.click addTo(current build).removeBuild
-  _______________________________
-6)  NVIDIA RTX 3080Ti: $5000   	
-    on.click addTo(current build).removeBuild
-*/
-
-//                                      Processors 
-/*
-1) AMD ryzen 3:             $200
-2)AMD ryzen 5:              $300
-3)AMD ryzen 7:              $400
-4)AMD ryzen 9 :             $800
-5)AMD threadripper 3960x :  $1500
-6) THA SHREDDER:            $5000
-*/
-
-//                                     Motherboards 
-/*
-1)ASUS b450:        $150 
-2)ASUS b550:        $250
-3)ASUS x570:        $350
-4)ASUS Godlike 570: $700
-5)AORUS XTREME:     $2000
-6)MEGAVBOARD:       $5000
-
-
-*/
-
-
-
-
-
-
-/*        := Checkout =:
-When you click ‘checkout’ button,
- A prompt will appear:		“Finished shopping?”
-UserResponseInput  : “yes”, “no” (or on.click ok/cancel)
-After purchase:
-Alert: “Good luck mining!”
- */
-
-
-
-
-
-
+function get(){
+    for( i=1; i <= 18;i++){
+        let item = (document.getElementById("text" +i));
+        let returnItem = localStorage.getItem("storedItem" +i);
+        item.value = returnItem;
+        console.log(returnItem);
+    }
+}
+get();
 
 })
