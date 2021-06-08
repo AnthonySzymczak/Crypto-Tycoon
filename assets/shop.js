87a83ad8cb99a6ebd7dc0d21e3989e4c9fdb1d02
AnthonySzymczak <doyalajs1@live.com>
2021-06-08 16:19:36 -0400

    appending values to ID's

--- assets/shop.js
+++ assets/shop.js
@@ -38,27 +38,37 @@ get();
 //  VIDEO CARDS
 //
 let usdConversion = [];
-document.getElementById('1').val='125';
-document.getElementById('2').val='300';
-document.getElementById('3').val='650';
-document.getElementById('4').val='1300';
-document.getElementById('5').val='2600';
-document.getElementById('6').val='5000';
+document.getElementById('v1').append.val='125';
+document.getElementById('2').append.val='300';
+document.getElementById('3').append.val='650';
+document.getElementById('4').append.val='1300';
+document.getElementById('5').append.val='2600';
+document.getElementById('6').append.val='5000';
 //
 //  PROCESSORS
 //
-document.getElementById('7').val='200';
-document.getElementById('8').val='300';
-document.getElementById('9').val='400';
-document.getElementById('10').val='800';
-document.getElementById('11').val='1500';
-document.getElementById('12').val='5000';
+document.getElementById('7').append.val='200';
+document.getElementById('8').append.val='300';
+document.getElementById('9').append.val='400';
+document.getElementById('10').append.val='800';
+document.getElementById('11').append.val='1500';
+document.getElementById('12').append.val='5000';
 //
 //  MOTHER BOARDS
 //
-document.getElementById('13').val='150';
-document.getElementById('14').val='250';
-document.getElementById('15').val='350';
-document.getElementById('16').val='700';
-document.getElementById('17').val='2000';
-document.getElementById('18').val='5000';
+document.getElementById('13').append.val='150';
+document.getElementById('14').append.val='250';
+document.getElementById('15').append.val='350';
+document.getElementById('16').append.val='700';
+document.getElementById('17').append.val='2000';
+document.getElementById('18').append.val='5000';
+
+
+/*
+<button onclick="subtractUSD()"></button>
+function subtractUSD() {
+    for (i = usdConversion; 1 <= 18; val--) {
+        
+    }
+}
+*/
