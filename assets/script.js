2e75cf5ee782d3ac52fd2d6900253807747ea168
Hamzah Ullah <82611798+brobro10000@users.noreply.github.com>
2021-06-08 15:45:07 -0400

    Feature/marquee (#24)
    * Change where testHeader would be called to properly load the data into click and usdconversion
    
    * Basic Marquee created pulling API data
    
    * Added change in price color to marquee  value
    
    * Inline flex on inner html of marquee added with flex
    
    * Added Doge coin image in ticker with styling
    
    * Added rest of the ticker elements with 1h and 24h changes
    
    * Added components for twitter feed
    
    * Dynamic Tweets Created
    
    * Formatting of marquee2 to appear better on page

--- assets/script.js
+++ assets/script.js
@@ -4,31 +4,25 @@ const API_KEY = "0a4a30fd69c551af6529573e0770da441e7496f29fade17e52b1b78e221a344
 const CURRENCIES = "USD"; 
 const DOGE_HASHRATE = .25
 const SAVED_VARIABLES = 2
-const CRYPTOCURRENCIES = "DOGE,ETH,BTC";
+const CRYPTOCURRENCIES_CONVERSION = "DOGE,ETH,BTC";
+const CRYPTOCURRENCIES_TREND = "doge-dogecoin,eth-ethereum,btc-bitcoin"
 const DECIMAL_POINTS = 2
+const TWEETS = 3;
 //JQUERY ID REFERENCES
 var strtbtn = $("#startGame")
 var strtPage= $("#crypto")
 var gamePage = $("#gamePage")
 var shopPage = $("#shopContainer")
 
-//API KEYS
-var CRYPTO_USD_PRICE_API = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${CRYPTOCURRENCIES}&tsyms=${CURRENCIES}&api_key=${API_KEY}`
-var USD_CRYPTO_PRICE_API = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${CURRENCIES}&tsyms=${CRYPTOCURRENCIES}&api_key=${API_KEY}`
+//API KEYS, APIs USED, Cryptocompare, Coinpaprika
+var CRYPTO_USD_PRICE_API = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${CRYPTOCURRENCIES_CONVERSION.split(",")[0]}&tsyms=${CURRENCIES}&api_key=${API_KEY}`
+var CRYPTO_TREND_API = `https://api.coinpaprika.com/v1/tickers/${CRYPTOCURRENCIES_TREND.split(",")[0]}/`
+var CRYPTO_TWITTER_API = `https://api.coinpaprika.com/v1/coins/${CRYPTOCURRENCIES_TREND.split(",")[0]}/twitter`
 //VARIABLES FOR CONVERSION
 
 var usdConversion = 0
 var cryptoHeld = 0
 var clickCount=0
-//CHECKS IF LOCAL STORAGE POPULATED ELSE ZERO
-if(localStorage.length< SAVED_VARIABLES)
-{
-    var usdConversion = 0
-    var clickCount=0
-} else {
-    usdConversion = loadUSDConversion()
-    clickCount = loadClicks()
-}
 
 gamePage.hide()
 shopPage.hide()
@@ -38,6 +32,58 @@ function loadGamePage() {
     strtPage.hide()
     gamePage.show()
     // shopPage.show()
+}
+
+function cryptoTwitter(CRYPTO_TWITTER_API){
+    fetch(CRYPTO_TWITTER_API).then(function(response){
+        return response.json()
+    }).then(function(data){
+        console.log(data)
+        for(var i = 0;i<TWEETS;i++){
+        const {date,like_count,media_link,retweet_count,status,user_image_link,user_name} = data[i]
+        console.log( date,like_count,media_link,retweet_count,status,user_image_link,user_name)
+        twitterFeed(i,{date,like_count,media_link,retweet_count,status,user_image_link,user_name})
+        }
+    })
+}
+function cryptoTrends(CRYPTO_TREND_API){
+    fetch(CRYPTO_TREND_API).then(function(response){
+        return response.json()
+    }).then(function (data) {
+        console.log(data)
+        const {percent_change_15m,percent_change_1h,percent_change_24h} = data.quotes.USD
+        dogeTicker({percent_change_15m,percent_change_1h,percent_change_24h})
+    })
+}
+function dogeTicker(data) {
+    $("#innerMarquee15m").text(data.percent_change_15m + "%")
+    $("#innerMarquee1h").text(data.percent_change_1h + "%")
+    $("#innerMarquee1d").text(data.percent_change_24h + "%")
+        if(data.percent_change_15m > 0)
+        {
+            $("#innerMarquee15m").removeClass("negativeChange neutralChange").addClass("positiveChange")
+        } else if(data.percent_change_15m < 0) {
+            $("#innerMarquee15m").removeClass("positiveChange neutralChange").addClass("negativeChange")
+        } else {
+            $("#innerMarquee15m").removeClass("positiveChange negativeChange").addClass("neutralChange")
+        }
+        if(data.percent_change_1h > 0)
+        {
+            $("#innerMarquee1h").removeClass("negativeChange neutralChange").addClass("positiveChange")
+        } else if(data.percent_change_1h < 0) {
+            $("#innerMarquee1h").removeClass("positiveChange neutralChange").addClass("negativeChange")
+        } else {
+            $("#innerMarquee1h").removeClass("positiveChange negativeChange").addClass("neutralChange")
+        }
+        if(data.percent_change_24h > 0)
+        {
+            $("#innerMarquee1d").removeClass("negativeChange neutralChange").addClass("positiveChange")
+        } else if(data.percent_change_24h < 0) {
+            $("#innerMarquee1d").removeClass("positiveChange neutralChange").addClass("negativeChange")
+        } else {
+            $("#innerMarquee1d").removeClass("positiveChange negativeChange").addClass("neutralChange")
+        }
+        
 }
 //FETCH API THAT CONVERTS ON CLICK AND RETURNS SUM OF cryptoHeld + usdConversion
 //ALSO SHOWS AMOUNT CONVERTED ON CLICK AND CURRENT DOGE PRICE
@@ -93,6 +139,24 @@ function testHeader() {
     $("<h1>").text("Convert to USD").attr({"id":"convert"}).appendTo("#headerContainer")
     $("<h1>").text("Save").attr({"id":"save"}).appendTo("#headerContainer")
 }
+function twitterFeed(i,data){
+    console.log(data)
+    $("<div>").attr({"id":"twitterContainer"+i, "class":"card"}).appendTo("#marquee2")
+    $("<header>").attr({"id":"twitterHeader"+i,"class":"card-header"}).appendTo("#twitterContainer"+i)
+    $("<img>").attr({"id":"twitterHeaderIcon"+i,"class":"card-header-icon", "src": data.user_image_link}).appendTo("#twitterHeader"+i)
+    $("<p>").text(data.user_name).attr({"id":"twitterHeaderTitle"+i,"class":"card-header-title"}).appendTo("#twitterHeader"+i)
+    $("<div>").attr({"id":"twitterContentContainer"+i, "class":"card-content"}).appendTo("#twitterContainer"+i)
+    $("<p>").text(data.status).attr({"id":"twitterContent"+i,"class":"content"}).appendTo("#twitterContentContainer"+i)
+    twitterMedia(i, data)
+    $("<footer>").attr({"id":"twitterFooterContainer"+i,"class":"card-footer"}).appendTo("#twitterContainer"+i)
+    $("<p>").text("Retweets:" + data.retweet_count).attr({"id":"twitterRT"+i, "class":"card-footer-item"}).appendTo("#twitterFooterContainer"+i)
+    $("<p>").text("Likes:" + data.like_count).attr({"id":"twitterFooterLikes"+i,"class":"card-footer-item"}).appendTo("#twitterFooterContainer"+i)
+    $("<p>").text(data.date).attr({"id":"twitterFooterDate"+i,"class":"card-footer"}).appendTo("#twitterContainer"+i)   
+}
+function twitterMedia(i){
+    $("<video>").attr({"id":"twitterVideo", "class":"card-image", "src":"https://video.twimg.com/tweet_video/E2-xIxeVUAMRCDx.mp4"}).appendTo("#twitterContentContainer"+i)
+    $("<img>").attr({"id":"twitterImage", "class":"card-image", "src":""}).appendTo("#twitterContainer")
+}
 //UPDATES ON CLICK AMOUNT, FUTURE IMAGE REFERENCE
 function updateCount(){
     cryptoHeld = convertDoge(clickCount)
@@ -125,9 +189,24 @@ function resetValues() {
 }
 
 //CALLS TEST HEADER, LOADS DATA FROM LOCALSTORAGE AND BUTTONS FOR TEST HEADER
-testHeader()
-loadCurrency(clickCount,usdConversion)
+//CHECKS IF LOCAL STORAGE POPULATED ELSE ZERO
+if(localStorage.length< SAVED_VARIABLES)
+{
+    var usdConversion = 0
+    var clickCount=0
 
+} else {
+    console.log(clickCount,usdConversion)
+    loadCurrency(clickCount,usdConversion)
+ 
+}
+testHeader()
+cryptoTwitter(CRYPTO_TWITTER_API)
+cryptoTrends(CRYPTO_TREND_API)
+
+// setInterval(function(){
+//     cryptoTrends(CRYPTO_TREND_API)
+// },15000)
 //INCREMENTS CRYPTOHELD
 $("#increment").on("click",function(event){
     event.preventDefault()
