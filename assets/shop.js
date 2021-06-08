9605dafe661f91444012c8d127801eba9383a9bc
AnthonySzymczak <doyalajs1@live.com>
2021-06-08 15:54:27 -0400

    adding set values to id's

--- assets/shop.js
+++ assets/shop.js
@@ -33,3 +33,32 @@ function get(){
 }
 get();
 */
+
+//
+//  VIDEO CARDS
+//
+let usdConversion = [];
+document.getElementById('1').val='125';
+document.getElementById('2').val='300';
+document.getElementById('3').val='650';
+document.getElementById('4').val='1300';
+document.getElementById('5').val='2600';
+document.getElementById('6').val='5000';
+//
+//  PROCESSORS
+//
+document.getElementById('7').val='200';
+document.getElementById('8').val='300';
+document.getElementById('9').val='400';
+document.getElementById('10').val='800';
+document.getElementById('11').val='1500';
+document.getElementById('12').val='5000';
+//
+//  MOTHER BOARDS
+//
+document.getElementById('13').val='150';
+document.getElementById('14').val='250';
+document.getElementById('15').val='350';
+document.getElementById('16').val='700';
+document.getElementById('17').val='2000';
+document.getElementById('18').val='5000';
